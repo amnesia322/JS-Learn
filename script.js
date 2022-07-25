cf6f26e9d672a8550f53e60b2d2590e3721dc7b1
amnesia322 <dev.amnesia322@gmail.com>
2022-07-25 18:28:30 +0300

    study conditions

--- script.js
+++ script.js
@@ -2,3 +2,59 @@ let routeDistance = 78;
 let averageSpeed = 80;
 
 let routeTime = Math.round((routeDistance * 1.6) / averageSpeed);
+
+// Условия. подсчет времени прогулки 
+
+let optimalTime = 80;
+let walkTime = 20;
+let timeLeft;
+
+if (walkTime >= optimalTime) {
+   timeLeft = 0;
+} else {
+   timeLeft = optimalTime - walkTime;
+}
+console.log('Осталось гулять ещё ' + timeLeft + ' минут.');
+
+
+// Условия. Логические "И", "Или"
+
+let enoughDevelopers = false;
+let techAvailable = false;
+let onVacation = true;
+let onSickLeave = true;
+
+if (enoughDevelopers && techAvailable) {
+   console.log('Проект можно выполнить');
+}
+
+if (!onVacation) {
+   console.log('Никого нет в отпуске');
+} else {
+   console.log('Кто-то в отпуске');
+}
+
+if (!onSickLeave) {
+   console.log('Никого нет на больничном');
+} else {
+   console.log('Кто-то на больничном');
+}
+
+// тест по условиям 
+
+let temperature = 20;
+let isRaining = true;
+let minutes = 0;
+
+if (isRaining || temperature < 10 || temperature > 35) {
+   minutes = 0;
+} else if (temperature >= 10 && temperature < 15) {
+   minutes = 30;
+} else if (temperature >= 15 && temperature < 25) {
+   minutes = 40;
+} else if (temperature >= 25 && temperature <= 35) {
+   minutes = 20;
+}
+
+
+
