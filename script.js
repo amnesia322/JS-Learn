528dda6dfeef19d423ea3b8c50a782bc6bceaf33
amnesia322 <dev.amnesia322@gmail.com>
2022-07-26 15:56:57 +0300

    finish terms

--- script.js
+++ script.js
@@ -74,3 +74,20 @@ if (!divisionThree && !divisionFive) {
    taskResult = number;
 };
 
+// goToMarket 
+
+let time = 15;
+
+let goToDairy = false;
+let goToStore = false;
+let goToMarket = false;
+
+if (time >= 8 && time < 13 || time >= 14 && time < 19) {
+   goToDairy = true;
+} else if (time >= 13 && time < 14) {
+   goToStore = true;
+} else if (time >= 7 && time < 20) {
+   goToMarket = true;
+}
+
+
