f7d92ff8f27efb7f78310abb21dce5a5dd103463
amnesia322 <dev.amnesia322@gmail.com>
2022-07-29 17:40:49 +0300

    add getPrice

--- calculate-miles.js
+++ calculate-miles.js
@@ -43,5 +43,19 @@ let calculateSalary = function (salary) {
 };
 
 
+//Расчет стоимости проекта 
+
+let getPrice = function (time, isQuickly) {
+   let rate = 1500;
+   if (isQuickly) {
+      rate *= 2.5;
+      time /= 2;
+   }
+   if (time > 150) {
+      rate -= 250;
+   }
+   return time * rate;
+};
+
 
 
