21bce76cf2445b5a83a98a75d64f03d7f96cba96
amnesia322 <dev.amnesia322@gmail.com>
2022-08-10 11:47:23 +0300

    add function makeElement

--- create-element/script.js
+++ create-element/script.js
@@ -1,17 +1,21 @@
+var makeElement = function (tagName, className, text) {
+    var element = document.createElement(tagName);
+    element.classList.add(className);
+    if (text) {
+        element.textContent = text;
+    }
+    return element;
+};
+
 var cardList = document.querySelector('.products');
 console.log(cardList.children);
 
-var listItem = document.createElement('li');
-listItem.classList.add('product');
+var listItem = makeElement('li', 'product');
 cardList.appendChild(listItem);
 console.log(cardList.children);
 
-var title = document.createElement('h2');
-title.classList.add('product__title');
-title.textContent = 'Профессиональная селфи-палка';
+var title = makeElement('h2', 'product__title', 'Профессиональная селфи-палка');
 listItem.appendChild(title);
 
-var price = document.createElement('p');
-price.classList.add('product__price');
-price.textContent = '1000';
+var price = makeElement('p', 'product__price', '1000');
 listItem.appendChild(price);
